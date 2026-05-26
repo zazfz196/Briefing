038bcba01ec924c5dd978dc19be690a21c7e8789
zazfz196 <pepecoin1109@gmail.com>
2026-05-26 15:21:52 -0300

    guixu niguinha

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
 // senha default será entregue junto ao projeto(pode ser alterada pelo dono do site, LeoTatu)
-$SENHA = '********';
+$SENHA = 'Jexu niger';
 
 session_start();
 session_regenerate_id(true);
